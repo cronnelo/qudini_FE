39da96254e136d2dc2bc833efa2318bcf6067064
Ronnel Ocampo <ocampo.ronnelc@gmail.com>
2017-09-17 12:42:50 +0800

    Optimze assets
    * Add gulp
    * Minify and concat JS and CSS

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var gutil = require('gulp-util')
+var uglify = require('gulp-uglify')
+var concat = require('gulp-concat')
+var minifyCSS = require('gulp-csso');
+
+gulp.task('js', function () {
+  gulp.src([
+    'public/bower_components/angularjs/angular.js',
+    'public/QueueApp.js',
+    'public/add-customer/*.js',
+    'public/customer/*.js'
+  ])
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('sass', function() {
+  gulp.src('public/css/main.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('build', ['sass', 'js'])
